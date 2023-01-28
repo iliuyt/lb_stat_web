0708825c421da15245eef1c99b7dd9d1d24bf964
plbbzd <yuntao_liu.sgmext@saic-gm.com>
2023-01-28 10:01:47 +0800

    优化

--- vue.config.js
+++ vue.config.js
@@ -19,19 +19,19 @@ if (process.env.NODE_ENV === "prod") {
 module.exports = {
     lintOnSave: false,
     pages: {
-        index: {
+        home: {
             // page 的入口
-            entry: "src/pages/index/main.js",
+            entry: "src/pages/home/main.js",
             // 模板来源
-            template: "public/index.html",
+            template: "public/home.html",
             // 在 dist 的输出
-            filename: "index.html",
+            filename: "home.html",
             // 当使用 title 选项时，
             // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
             title: "",
             // 在这个页面中包含的块，默认情况下会包含
             // 提取出来的通用 chunk 和 vendor chunk。
-            chunks: ["chunk-vendors", "chunk-common", "index"],
+            chunks: ["chunk-vendors", "chunk-common", "home"],
         },
         other: {
             // page 的入口
